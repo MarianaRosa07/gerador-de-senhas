18d84417c39ce85713c3b9decc153012eab9c42a
Mariana Rosa <marianarosa@pop-os.localdomain>
2023-11-14 17:33:28 -0300

    cria js, eslint e style

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import { nanoid } from 'nanoid';
-import copy from 'clipboard-copy';
+// console.log(nanoid());
+
+// import copy from 'clipboard-copy';
 import './style.css';
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
+
 passwordBtnEl.addEventListener('click', () => {
-  const randomPassword = nanoid();
-  displayPasswordEl.innerHTML = randomPassword;
+    const randomPassword = nanoid();
+    displayPasswordEl.innerHTML = randomPassword;
+    console.log(randomPassword);
 });
 
-displayPasswordEl.addEventListener('click', () => {
-  copy(event.target.innerHTML);
-  alert('Senha copiada!');
-});
+// displayPasswordEl.addEventListener('click', () => {
+//   copy(event.target.innerHTML);
+//   alert('Senha copiada!');
+// });
